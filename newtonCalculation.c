ade6934d89165e7913b3d7fada60e798f5ab7257
brewerj3 <brewerj3@hawaii.edu.com>
2022-09-03 16:27:59 -1000

    added newtonCalculation.h

---
+++ newtonCalculation.c
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  untitled1 - EE 205 - Spr 2022
+///
+/// @file newtonCalculation.c
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date   03_Sep_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "newtonCalculation.h"
---
+++ newtonCalculation.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  untitled1 - EE 205 - Spr 2022
+///
+/// @file newtonCalculation.h
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date   03_Sep_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef UNTITLED1_NEWTONCALCULATION_H
+#define UNTITLED1_NEWTONCALCULATION_H
+
+const double angleOfGunInDegrees = 41; //Angles
+const double initialVelocity = 823;  //Meters per Second
+
+double currentHorizontalDistance = 0;
+
+
+
+#endif //UNTITLED1_NEWTONCALCULATION_H
